b6ffcab5bacdc66b776ef3934c62080b106491ea
Sobhan Zadehali <99137872+sobhanzadehali@users.noreply.github.com>
2024-07-31 01:52:18 +0330

    add-customuser-class-and-tests
    custom user created and basic test was successful

--- core/account/tests.py
+++ core/account/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+import pytest
+
+User = get_user_model()
+
 
 # Create your tests here.
+
+
+@pytest.mark.django_db
+def test_user_creation():
+    user = User.objects.create_superuser('09380456987', 'password', )
+    assert user.is_superuser == True
+    assert user.is_staff == True
+    assert user.is_active == True
+    print(user.phone)
+    print(user.is_superuser)
+    assert user.phone == '09380456987'
