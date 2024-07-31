2ee388bf57fddfc9552eeeb30a34a8719b169bb4
Sobhan Zadehali <99137872+sobhanzadehali@users.noreply.github.com>
2024-07-31 10:16:17 -0700

    add-shop-app

--- core/core/settings.py
+++ core/core/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'account.apps.AccountConfig',
+    'shop.apps.ShopConfig',
 ]
 
 MIDDLEWARE = [
@@ -116,7 +117,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 
 # Custom User model
 AUTH_USER_MODEL = 'account.CustomUser'
