670cd85174ba027176e2caabfbe4923eedbcae52
Sobhan Zadehali <99137872+sobhanzadehali@users.noreply.github.com>
2024-08-06 11:50:44 +0330

    add-jwt-endpoints

--- core/account/api/v1/urls.py
+++ core/account/api/v1/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from rest_framework.authtoken import views as rest_views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from . import views
 
 app_name = 'api-v1'
@@ -11,4 +15,7 @@ urlpatterns = [
     path('token/login/', rest_views.ObtainAuthToken.as_view(), name='user-token-login'),
     # logout
     path('token/logout/', views.TokenLogoutView.as_view(), name='user-token-logout'),
+    # JWT
+    path('jwt/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('jwt/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- core/core/settings.py
+++ core/core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from django.utils.timezone import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -138,7 +139,13 @@ AUTH_USER_MODEL = 'account.CustomUser'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
     ]
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
