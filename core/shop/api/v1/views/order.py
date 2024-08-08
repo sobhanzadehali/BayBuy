205c7898b3acfae275250cf01a3c5bc90c38793a
Sobhan Zadehali <99137872+sobhanzadehali@users.noreply.github.com>
2024-08-08 21:01:12 +0330

    add-small docs

--- core/shop/api/v1/views/order.py
+++ core/shop/api/v1/views/order.py
@@ -6,12 +6,18 @@ from ..serializers.order import OrderSerializer, ItemSerializer
 
 
 class OrderViewSet(ModelViewSet):
+    """
+    viewset of Order model
+    """
     serializer_class = OrderSerializer
     permission_classes = (IsAdminUser, OrderPermission)
     queryset = Order.objects.all()
 
 
 class OrderItemViewSet(ModelViewSet):
+    """
+    viewset of OrderItem model
+    """
     serializer_class = ItemSerializer
     permission_classes = (IsAdminUser, OrderItemPermission)
     queryset = Item.objects.all()
